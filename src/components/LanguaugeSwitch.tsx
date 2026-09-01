3f95dbd3ff9a33a19d8db98239f8648e3789c73a
Xantus10 <jaroslav.zaba@post.cz>
2026-09-01 20:16:23 +0200

    Fix: Bug with init language setting

--- src/components/LanguaugeSwitch.tsx
+++ src/components/LanguaugeSwitch.tsx
@@ -7,8 +7,10 @@ export const SUPPORTED_LANGUAGES = ['cs', 'en'];
 function LanguageSwitch() {
   const { i18n } = useTranslation();
 
+  let lang = i18n.language.split('-')[0];
+
   return (
-    <select value={i18n.language} onChange={(e) => i18n.changeLanguage(e.currentTarget.value)}>
+    <select value={lang} onChange={(e) => i18n.changeLanguage(e.currentTarget.value)}>
       {
         SUPPORTED_LANGUAGES.map((val) => {
           return (
